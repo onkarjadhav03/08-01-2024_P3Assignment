8d7d5fb90716ff961fb90f8eeb4bf9ec20f2e0c5
onkar <jadhavonkar333@gmail.com>
2024-01-08 16:46:12 +0530

    assignment added

---
+++ ConsoleAppAsign2/UserAuthenticator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAsign2
+{
+    public class UserAuthenticator
+    {
+        private Dictionary<string, string> userDatabase; // Simulating a simple in-memory user database
+
+        public UserAuthenticator()
+        {
+            userDatabase = new Dictionary<string, string>();
+        }
+
+        public bool RegisterUser(string username, string password)
+        {
+            if (!userDatabase.ContainsKey(username))
+            {
+                userDatabase[username] = password;
+                return true;
+            }
+            return false; // User already exists
+        }
+
+        public bool LoginUser(string username, string password)
+        {
+            if (userDatabase.TryGetValue(username, out string storedPassword))
+            {
+                return password == storedPassword;
+            }
+            return false; // User not found
+        }
+
+        public bool ChangePassword(string username, string newPassword)
+        {
+            if (userDatabase.ContainsKey(username))
+            {
+                userDatabase[username] = newPassword;
+                return true;
+            }
+            return false; // User not found
+        }
+    }
+}
---
+++ ConsoleAppAsign2/UserAuthenticatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleAppAsign2
+{
+
+   
+
+    [TestFixture]
+    public class UserAuthenticatorTests
+    {
+        [Test]
+        public void TestUserRegistration()
+        {
+            UserAuthenticator authenticator = new UserAuthenticator();
+            Assert.IsTrue(authenticator.RegisterUser("user1", "password1"));
+            Assert.IsFalse(authenticator.RegisterUser("user1", "password2")); // Duplicate registration should fail
+        }
+
+        [Test]
+        public void TestUserLogin()
+        {
+            UserAuthenticator authenticator = new UserAuthenticator();
+            authenticator.RegisterUser("user1", "password1");
+
+            Assert.IsTrue(authenticator.LoginUser("user1", "password1"));
+            Assert.IsFalse(authenticator.LoginUser("user1", "wrongpassword"));
+            Assert.IsFalse(authenticator.LoginUser("nonexistentuser", "password1"));
+        }
+
+        [Test]
+        public void TestChangePassword()
+        {
+            UserAuthenticator authenticator = new UserAuthenticator();
+            authenticator.RegisterUser("user1", "password1");
+
+            Assert.IsTrue(authenticator.ChangePassword("user1", "newpassword"));
+            Assert.IsTrue(authenticator.LoginUser("user1", "newpassword"));
+            Assert.IsFalse(authenticator.ChangePassword("nonexistentuser", "newpassword"));
+        }
+    }
+}
